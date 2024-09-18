b2c2dd03298e6d5584ec94fffeb9997bf1c80e15
RaulPalomo <raul.palomo.7e7@itb.cat>
2024-09-18 23:42:26 +0200

    Projecte refactoritzacio

---
+++ RepasGit/RepasGit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int nota1, nota2,nota3;
+        const int MinNota = 6;
+
+        Console.WriteLine("Introdueix la primera qualificació");
+        nota1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introdueix la segona qualificació: ");
+        nota2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introdueix la tercera qualificació: ");
+        nota3 = Convert.ToInt32(Console.ReadLine());
+
+        float promig = (nota1 + nota2 + nota3) / 3.0f;
+
+        Console.WriteLine("El promig de les notes es: " + promig);
+        Console.WriteLine(promig > MinNota ? "Aprovat" : "Suspès");
+
+    }
+}
+
